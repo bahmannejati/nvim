e836986f4378785990f67931f2f8504045e37b43
Bahman Nejati <b.nejati@corp.mabnadp.com>
2023-12-26 15:36:39 +0330

    fix: force theme

--- 2nthony/.config/nvim/init.lua
+++ 2nthony/.config/nvim/init.lua
@@ -1,4 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd([[colorscheme tokyonight-night]])
 vim.opt.clipboard = 'unnamedplus'
