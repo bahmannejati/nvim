d7751b2120a58e31ae9a9a0c8374ddc03b0518df
Bahman Nejati <b.nejati212@gmail.com>
2024-07-07 17:40:40 +0330

    fix: add luaSnip to deps

--- 2nthony/.config/nvim/lua/plugins/core.lua
+++ 2nthony/.config/nvim/lua/plugins/core.lua
@@ -12,6 +12,11 @@ return {
     },
   },
   {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  },
+  {
     "akinsho/toggleterm.nvim",
     config = function()
       require("toggleterm").setup({
